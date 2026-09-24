6e168dfbb57404eb58b901245447309ca5ae635e
Haojun-Ethan <haojun.jia@uon.edu.au>
2026-09-24 16:14:46 +1000

    rewrite ---5-02

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 import { AuthProvider } from './context/AuthContext.jsx'
-import { runAuthApiSelfTest } from './api/test/auth.test.js'
 
 import App from './App.jsx'
 
-if (import.meta.env.DEV && import.meta.env.VITE_RUN_API_SELF_TEST === 'true') {
-  runAuthApiSelfTest();
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
